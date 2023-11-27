5905de6f2a0ba18f7e81f0a18338927d9f05fcb7
Palacios1416 <130609254+Palacios1416@users.noreply.github.com>
2023-11-27 14:05:12 -0600

    advancing a little

--- EjerciciosPOO/Three/App.java
+++ EjerciciosPOO/Three/App.java
@@ -2,30 +2,72 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class App {
+    public static int count, edad;
+    public static double dinero;
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
         SalaCine sala = new SalaCine(8, 9);
 
-        // Crear una película y reproducirla
         Pelicula pelicula = new Pelicula("El Gran Cine", 120, 13, "Director A", 10.0);
         sala.reproducirPelicula(pelicula);
 
-        // Solicitar información al usuario para vender una entrada
         System.out.println("Ingrese su nombre: ");
         String nombre = scanner.nextLine();
 
         System.out.println("Ingrese su edad: ");
-        int edad = scanner.nextInt();
+
+        while (count < 3) {
+                try {
+                    do {
+                       edad = scanner.nextInt();
+                        if ((edad < 1 || edad > 3) && count < 3) {
+                            System.out.println("El valor ingresado no es logico");
+                            System.out.println("Ingrese su edad otra vez: ");
+                            count++;
+                        }
+                        if(count == 3){
+                            break;
+                        }
+                    } while ((edad < 1 || edad > 3) && count != 3);
+                    break;
+                } catch (InputMismatchException e) {
+                    System.out.println("No puede ingresar un valor no numerico.");
+                    count++;
+                }
+            }
+            if(count == 3){
+                    System.out.println("Demasiados intentos, el programa termino");
+            }
 
         System.out.println("Ingrese su sexo (M/F): ");
         char sexo = scanner.next().charAt(0);
 
         System.out.println("Ingrese la cantidad de dinero que trae: ");
-        double dinero = scanner.nextDouble();
 
-        // Solicitar asiento al usuario
+        while (count < 3) {
+            			try {                
+                			do {
+                    				dinero = scanner.nextDouble();
+                    			if (dinero <= 0) {
+                                		System.out.println("Necesita mas dinero");
+                                		System.out.print("Si se equivoco, ingrese la cantidad de dinero que trae otra vez: ");
+                            		}
+                			} while (dinero <= 0);
+                		break;
+            			} catch (InputMismatchException e) {
+                			System.out.println("Valor incorrecto, intente nuevamente.");
+                			scanner.nextLine();
+                			count++;
+            			}
+        		}
+        		if(count == 3){
+            			System.out.println("Se agotaron los intentos.");
+        		}else {
+            			cuenta.depositar(cantidadDeposito);        
+			    }
+
         System.out.println("Escoja su asiento (por ejemplo, A1): ");
         String asientoSeleccionado = scanner.next().toUpperCase();
         char fila = asientoSeleccionado.charAt(0);
@@ -39,13 +81,12 @@ public class App {
         scanner.close();
 
 
-    //     SalaCine sala = new SalaCine(8, 9);
 
-    //     // Crear una película y reproducirla
-    //     Pelicula pelicula = new Pelicula("El Gran Cine", 120, 13, "Director A", 10.0);
+    //     // se crea una pelicula y la reproduce
+    //     Pelicula pelicula = new Pelicula("El Gran Cine", 120, 13, "Director Guillermo del toro", 10.0);
     //     sala.reproducirPelicula(pelicula);
 
-    //     // Crear espectadores aleatorios y vender entradas
+    //     // espectadores aleatorios y se vende entradas
     //     for (int i = 0; i < 15; i++) {
     //         Espectador espectador = generarEspectadorAleatorio();
     //         Asiento asiento = obtenerAsientoAleatorio(sala);
